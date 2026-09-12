59e963839d56bfce2acc29f66cd0573769f131d8
Rajneesh Babu <rajneeshbabu@iisc.ac.in>
2026-09-12 23:27:28 +0530

    Add: copy-linked-list-with-random-pointer - submission-4

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-4.py
@@ -0,0 +1,55 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        # curr=head
+        # while curr:
+        #     cloned=Node(curr.val)
+        #     cloned.next=curr.next
+        #     curr.next=cloned
+        #     curr=cloned.next
+
+        # curr=head
+        # while curr:
+        #     if curr.random:
+        #         curr.next.random=curr.random.next
+        #     curr=curr.next.next
+
+        # dummy=Node(-1)
+        # tail=dummy
+        # curr=head
+        # while curr:
+        #     cloned=curr.next
+            
+        #     tail.next = cloned
+        #     tail = tail.next         
+        #     curr.next=cloned.next
+        #     curr = curr.next
+        # return dummy.next
+
+        otn = {None: None}
+
+        curr=head
+        while curr:
+            otn[curr]=Node(curr.val)
+            curr=curr.next
+        
+        curr=head
+        while curr:
+            cloned=otn[curr]
+            cloned.next=otn[curr.next]
+            cloned.random=otn[curr.random]
+            curr=curr.next
+        return otn[head]
+        
+        
